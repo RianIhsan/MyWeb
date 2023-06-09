3bc47d06b84cf516be532f469308bda5640aad0c
Reon is Rian <93025581+RianIhsan@users.noreply.github.com>
2023-06-09 20:13:04 +0700

    Update AboutSection.tsx

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -21,7 +21,7 @@ const AboutSection = () => {
       <div className="my-12 pb-12 md:pt-16 md:pb-48">
         <h1 className="text-center font-bold text-4xl">
           About Me
-          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+          <hr className="w-6 h-1 mx-auto my-4 bg-sky-500 border-0 rounded"></hr>
         </h1>
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
